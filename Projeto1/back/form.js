165ae119ad1fa9c30b3cee769fca4b692c213585
Marcelo Sena <marcelose15.ms@gmail.com>
2022-10-04 20:57:41 -0300

    Att 1.0

--- Projeto1/back/form.js
+++ Projeto1/back/form.js
@@ -1,7 +1,21 @@
-const btn = document.querySelector("#button-send");
+/* const btn = document.querySelector("#button-send");
 
 btn.addEventListener("click", function(e) {
 
     e.preventDefault();
     
 })
+
+const form = document.querySelector("formulary"); 
+
+const data = [
+    name = value.nome,
+    email = value.email,
+    contato = value.contato,
+    objetivo = value.objetivo,
+    solucão = value.solucao,
+    plataforma = value.plataforma,
+    searching = value.searching
+];
+
+console.log(typeof data) */
